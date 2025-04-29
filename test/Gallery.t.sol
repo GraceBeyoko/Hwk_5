e9000f19c73f28f78ad666b67589830fdaec390c
GraceBeyoko <144108289+GraceBeyoko@users.noreply.github.com>
2025-04-29 17:30:46 +0200

    Update Gallery.t.sol

--- test/Gallery.t.sol
+++ test/Gallery.t.sol
@@ -39,7 +39,7 @@ contract GalleryTest is Test {
         core.createProposal("Test Proposal", callData, GalleryCore.ProposalType.Routine);
         
         // Verify that the proposal is created successfully
-        GovernanceCore.ProposalState state = core.getProposalState(0);
+        GalleryCore.ProposalState state = core.getProposalState(0);
         assertEq(uint8(state), uint8(GalleryCore.ProposalState.Pending), "Proposal state should be Pending");
         vm.stopPrank();
     }
